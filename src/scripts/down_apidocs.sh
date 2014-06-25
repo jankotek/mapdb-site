5373c07c8efb69aad59fb2248091af4d0f71098e
stekot <stepan.kotek@gmail.com>
2014-06-25 19:53:52 +0200

    Version of javadoc is an argument.

--- src/scripts/down_apidocs.sh
+++ src/scripts/down_apidocs.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-mkdir /tmp/$$; wget http://search.maven.org/remotecontent?filepath=org/mapdb/mapdb/1.0.3/mapdb-1.0.3-javadoc.jar -O /tmp/$$-javadocs.jar;
+mkdir /tmp/$$;
+wget http://search.maven.org/remotecontent?filepath=org/mapdb/mapdb/$1/mapdb-$1-javadoc.jar -O /tmp/$$-javadocs.jar;
 mkdir target
 mkdir target/site
 mkdir target/site/apidocs
 cd target/site/apidocs;
 jar xf /tmp/$$-javadocs.jar
 rm /tmp/$$-javadocs.jar
+
+
