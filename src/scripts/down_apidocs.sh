c31cee26ea4fe0b9d42f5f73aa0c76855df67264
Jan Kotek <jan@kotek.net>
2014-11-04 18:38:20 +0200

    Read-the-docs preparation

--- src/scripts/down_apidocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget http://search.maven.org/remotecontent?filepath=org/mapdb/mapdb/$1/mapdb-$1-javadoc.jar -O /tmp/$$-javadocs.jar;
-mkdir target/site/apidocs
-cd target/site/apidocs;
-jar xf /tmp/$$-javadocs.jar
-rm /tmp/$$-javadocs.jar
--- src/scripts/pre-site.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rf target/site/apidocs
-if [ -a target/site/blog/index.html]; then rm target/site/blog/index.html; fi;
-if [ -a target/site/news.xml]; then rm target/site/news.xml; fi;
