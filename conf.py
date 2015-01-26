b7f2ffbcce856542f20fc6a6b08cf5a49f547f3c
Jan Kotek <jan@kotek.net>
2015-01-26 17:30:06 +0200

    Change theme, add publish script

--- conf.py
+++ conf.py
@@ -99,14 +99,14 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "rightsidebar": "false",
-    "relbarbgcolor": "black"
+#    "rightsidebar": "false",
+#    "relbarbgcolor": "black"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
