b5350c8561e55f693afdb572cd060c5aacbf2d31
Jan Kotek <jan@kotek.net>
2016-03-03 19:09:30 +0200

    Make: script to generate HTML site

---
+++ make.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# terminate script if any command fails
+set -e
+set -o pipefail
+
+# cleanup
+make clean
+# run maven tests in this project
+mvn clean test
+
+# make PDF manual
+(cd doc && make latexpdf)
+cp doc/_build/latex/MapDB.pdf down/mapdb-manual.pdf
+rm doc/_build -rf
+
+# create html site
+make html
+
+# create dokka documentation from ../mapdb
+(cd ../mapdb && mvn clean compile dokka:dokka)
+mkdir _build/html/dokka
+mv ../mapdb/target/dokka _build/html/dokka/lattest
